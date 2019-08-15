c601e207fbdced270b6c5b1dd054419110636832
Thomas Cayne <tcayne@parkplacetech.com>
2019-08-15 15:35:34 -0400

    initial app setup

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# AspNetCore3WebApi
-Practice demonstration Asp.Net Core 3 Web Api
+# Asp.Net Core 3 Web Api
+
+Practice demonstration of Asp.Net Core 3 Web Api
+
+- [Asp.Net Core 3 Web Api](#aspnet-core-3-web-api)
+  - [Roadmap](#roadmap)
+
+My intention is to eventually build this into a generic (template) full-fledge Asp dotNet Core web api starting with version 3 standards.
+
+## Roadmap
+
+Architecture:
+
+- [x] Integrate swagger api
+- [ ] Allow database connectionstring to be read from environment variables
+- [ ] Use Command Query Separation (CQS) pattern
+- [ ] Add Guard Clauses: [GuardClauses](https://github.com/ardalis/GuardClauses)
+- [ ] Automapper utilities: [Retrieving Property name from lambda expression](https://stackoverflow.com/a/52305334/939634)
+- [ ] Exception handler
+- [ ] Versioning
+- [ ] Token authentication: [overview](https://medium.com/@samueleresca/developing-token-authentication-using-asp-net-core-c3fbd7bfd7cb)
+- [ ] Authorize access
+- [ ] OData
+
+Funtionality:
---
+++ ThomascayneNetCoreWebApi/Startup.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ThomascayneNetCoreWebApi
+{
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    /// <summary>
+    /// The ConfigureServices method gets called by the runtime.Use this method to add services to the container.
+    /// </summary>
+    /// <param name="services"></param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      services.AddSwaggerGen(config => config.SwaggerDoc("v1", new OpenApiInfo { Title = "Thomas Cayne Web Api v1.0.0", Version = "v1" }));
+
+    }
+
+
+    /// <summary>
+    /// The Configure method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="env"></param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+      //var dbName = Configuration.GetSection("SQLDATABASENAME");
+
+      //Debug.WriteLine("\n\n\n\n--------->", dbName);
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseSwagger();
+
+      app.UseSwaggerUI(config =>
+      {
+        config.SwaggerEndpoint("/swagger/v1/swagger.json", "Thomas Cayne Web Api v1.0.0");
+      });
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
