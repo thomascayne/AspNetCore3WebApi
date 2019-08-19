36e2e53f3b67970b838cdea55428bd18ec4dfb02
Thomas Cayne <thomas.cayne@slashand.com>
2019-08-19 17:13:24 -0400

    add UseServiceProviderFactory

--- AspNetCore3WebApi/Program.cs
+++ AspNetCore3WebApi/Program.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AspNetCore3WebApi
 {
@@ -21,6 +22,8 @@ namespace AspNetCore3WebApi
             .ConfigureWebHostDefaults(webBuilder =>
             {
               webBuilder.UseStartup<Startup>();
-            });
+            })
+      .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+      ;
   }
 }
